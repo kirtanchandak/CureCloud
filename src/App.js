5c2196575875cdc7673d3293b7b6103c13745806
kirtanchandak <themarketingkirtan@gmail.com>
2023-02-22 23:07:40 +0530

    home setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
+import Home from "./components/Home";
 
 function App() {
   return (
-    <p className="text-3xl text-yellow-600 flex justify-center items-center">
-      Kirtan
-    </p>
+    <div>
+      <p className="text-3xl text-yellow-600 flex justify-center items-center">
+        Kirtan
+      </p>
+      <Home />
+    </div>
   );
 }
 
