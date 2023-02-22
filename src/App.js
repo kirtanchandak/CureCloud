167f611c2cbd151b7720ffaccac274fee36be6fb
kirtanchandak <themarketingkirtan@gmail.com>
2023-02-22 23:04:59 +0530

    app init

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <p className="text-3xl text-yellow-600 flex justify-center items-center">
+      Kirtan
+    </p>
+  );
+}
+
+export default App;
